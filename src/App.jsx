38e4cecf85b0b8019b4a4673c4deea7e61342bac
Your Name <deudajm@gmail.com>
2025-01-27 11:18:25 +0800

    batman

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <div className="border border-gray-500/20 p-5 rounded-sm min-w-full sm:min-w-80">
+          <h1 className="text-xl text-white font-semibold">Group Alarm</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
