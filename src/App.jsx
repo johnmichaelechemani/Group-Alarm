372dcb0250f5d844117d5c8eb7394be7adb6adf9
Your Name <deudajm@gmail.com>
2025-01-27 13:02:20 +0800

    `Refactor App component to include id fields and map over alarms and alarmsGroup arrays`

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,20 @@ function App() {
   const [enabled, setEnabled] = useState(false);
   const [alarms, setAlarms] = useState([
     {
-      title: "Alarm 1",
+      id: 1,
+      title: "First Alarm",
       enabled: true,
       alarmsGroup: [
         {
-          time: "04:50 PM",
+          id: 1,
+          time: "04:50",
           description: "wake up",
           label: "PM",
           enabled: true,
         },
         {
-          time: "11:10 AM",
+          id: 2,
+          time: "11:10",
           description: "lunch",
           label: "AM",
           enabled: false,
@@ -25,8 +28,25 @@ function App() {
       ],
     },
     {
-      title: "Alarm 2",
+      id: 2,
+      title: "Second Alarm",
       enabled: false,
+      alarmsGroup: [
+        {
+          id: 1,
+          time: "03:50",
+          description: "wake up",
+          label: "PM",
+          enabled: false,
+        },
+        {
+          id: 2,
+          time: "08:10",
+          description: "Sleep",
+          label: "PM",
+          enabled: false,
+        },
+      ],
     },
   ]);
 
@@ -35,37 +55,39 @@ function App() {
       <div className="flex justify-center items-center h-screen text-white">
         <div className="border border-gray-500/20 sm:p-5 p-1 rounded-sm min-w-full sm:min-w-96">
           <h1 className="text-xl text-white font-semibold">Group Alarm</h1>
-          <div className="border border-gray-500/20 p-2 rounded-sm my-2">
-            <div className="flex justify-between items-center">
-              <h1 className="text-sm text-white font-semibold">Alarm 1</h1>
-              <div className="flex justify-center items-center gap-2">
-                <MySwitch />
-                <MyModal />
-              </div>
-            </div>
-            <div>
-              <div
-                className={`my-2 py-2 px-2  rounded-lg text-xl border   flex justify-between items-center ${
-                  enabled
-                    ? "text-white border-gray-500/50 bg-blue-600/10"
-                    : "text-gray-700 border-gray-500/10 bg-gray-800/20"
-                }`}
-              >
-                <div>
-                  {" "}
-                  04:50 <span className="text-xs">PM </span>
+          {alarms.map((alarm, index) => (
+            <div
+              key={index}
+              className="border border-gray-500/20 p-2 rounded-sm my-2"
+            >
+              <div className="flex justify-between items-center">
+                <h1 className="text-sm text-white font-semibold">
+                  {alarm.title}
+                </h1>
+                <div className="flex justify-center items-center gap-2">
+                  <MySwitch />
+                  <MyModal />
                 </div>
-                <MySwitch enabled={enabled} setEnabled={setEnabled} />
               </div>
-              <div className="my-2 py-2 px-2 rounded-lg text-xl border border-gray-500/10 bg-gray-800/20 flex justify-between items-center">
-                <div>
-                  {" "}
-                  11:10 <span className="text-xs">AM</span>
+              {alarm.alarmsGroup.map((alarmGroup, index) => (
+                <div
+                  key={index}
+                  className={`my-2 py-2 px-2  rounded-lg text-xl border flex justify-between items-center ${
+                    enabled
+                      ? "text-white border-gray-500/50 bg-blue-600/10"
+                      : "text-gray-700 border-gray-500/10 bg-gray-800/20"
+                  }`}
+                >
+                  <div>
+                    {" "}
+                    {alarmGroup.time}{" "}
+                    <span className="text-xs">{alarmGroup.label} </span>
+                  </div>
+                  <MySwitch enabled={enabled} setEnabled={setEnabled} />
                 </div>
-                <MySwitch />
-              </div>
+              ))}
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
