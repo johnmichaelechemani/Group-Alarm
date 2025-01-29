1042b3943ae0cbd27b153d16ef28a659f90032c8
Your Name <deudajm@gmail.com>
2025-01-29 10:28:21 +0800

    `Added shadow effect to Notification component`

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -3,7 +3,7 @@ import { Icon } from "@iconify/react";
 export const Notification = () => {
   return (
     <>
-      <div className="border border-blue-500/20 bg-blue-500/10 p-2 rounded-lg flex justify-between items-center gap-1 ">
+      <div className="border border-blue-500/20 shadow-md shadow-blue-500/20 mb-2 bg-blue-500/10 p-2 rounded-lg flex justify-between items-center gap-1 ">
         <div className="flex items-start gap-1">
           <div>
             <Icon icon="mdi-light:clock" width="24" height="24" />
